1c9d60c4f49adad18f7801c52f41bfeb3bef6130
Tiago Cardoso <tiago.cardoso@inevo.pt>
2016-07-14 18:38:48 +0100

    adds rest api to get product price

--- src/main/java/org/nuxeo/workshop/products/webengine/Products.java
+++ src/main/java/org/nuxeo/workshop/products/webengine/Products.java
@@ -4,12 +4,19 @@
 
 package org.nuxeo.workshop.products.webengine;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
+import net.sf.json.JSONObject;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.webengine.model.WebObject;
 import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.workshop.products.ProductService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 
 /**
@@ -17,13 +24,29 @@ import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
  * @author inevo
  */
 @Path("/products")
-@Produces("text/html;charset=UTF-8")
+@Produces("application/json;charset=UTF-8")
 @WebObject(type="Products")
 public class Products extends ModuleRoot {
 
     @GET
-    public Object doGet() {
-        return getView("index");
+    @Path("computePrice/{id}/{country}")
+    @Produces("application/json;charset=UTF-8")
+    public Object getComputePrice(@PathParam("id") String id, @PathParam("country") String country) {
+        JSONObject json = new JSONObject();
+        DocumentModel product = ctx.getCoreSession().getDocument(new IdRef(id));
+        ProductService productService = Framework.getService(ProductService.class);
+        json.put("price", productService.computePriceWithTax(product, country));
+        return Response.ok(json.toString(2)).type("application/json").build();
     }
 
+    @GET
+    @Path("computePrice/{id}")
+    @Produces("application/json;charset=UTF-8")
+    public Object getComputePriceWithTax(@PathParam("id") String id) {
+        JSONObject json = new JSONObject();
+        DocumentModel product = ctx.getCoreSession().getDocument(new IdRef(id));
+        ProductService productService = Framework.getService(ProductService.class);
+        json.put("price", productService.computePrice(product));
+        return Response.ok(json.toString(2)).type("application/json").build();
+    }
 }
